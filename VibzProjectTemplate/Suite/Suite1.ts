0d38a692df9baf7e042ce35803bbd7d2224988c8
vibhuti.amit <vibhuti.amit@24074aae-3b9c-b3d1-3261-295945189422>
2010-12-02 17:19:28 +0000

    First Import

---
+++ VibzProjectTemplate/Suite/Suite1.ts
@@ -0,0 +1,8 @@
+<suite xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema">
+  <function name="Feature.Case1.main">
+    <data>
+      <var name="fileName"><![CDATA[C:/helloworld.txt]]></var>
+      <var name="message"><![CDATA[Hello World]]></var>
+    </data>
+  </function>
+</suite>
