1e56a61272491ccb3b3b1caaa500bb22cc3e5eb6
vibhuti.amit <vibhuti.amit@24074aae-3b9c-b3d1-3261-295945189422>
2011-01-27 19:44:17 +0000

    Google crawler

---
+++ Demo/GoogleCrawler/Suite/suite1.ts
@@ -0,0 +1,6 @@
+<?xml version="1.0" encoding="utf-8"?>
+<suite xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" name="suite1" ref="Suite/suite1">
+  <function name="tc1" ref="Script/case1/tc1">
+    <data />
+  </function>
+</suite>
