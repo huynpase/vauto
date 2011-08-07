94d535f8c4dc509d3590376b4f57f3de3a00bc32
vibzauto@gmail.com <vibzauto@gmail.com@24074aae-3b9c-b3d1-3261-295945189422>
2011-08-07 10:40:38 +0000

    Updated for Blog send test case

--- trunk/Demo/StockMarket/Suite/CompanyUpdate.ts
+++ trunk/Demo/StockMarket/Suite/CompanyUpdate.ts
@@ -2,17 +2,17 @@
 <suite xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" ref="Suite/CompanyUpdate" name="CompanyUpdate">
   <function ref="Script/case1/ProcessSourceStocks" name="ProcessSourceStocks">
     <data>
-      <var name="url" source="Internal" type="Scalar">
+      <var onerror="break" name="url" source="Internal" type="Scalar">
         <value><![CDATA[http://money.rediff.com/companies/{comp_name}/{comp_code}]]></value>
       </var>
-      <var name="stockSource" source="text" type="datatable">
+      <var onerror="break" name="stockSource" source="text" type="datatable">
         <param name="path">parse(concat(__currentpath,/company.txt))</param>
         <param name="rowseperationchar">parse(__newline)</param>
         <param name="colseperationchar">parse(__tab)</param>
         <value><![CDATA[]]></value>
       </var>
-      <var name="dataDestination" source="text" type="datatable">
-        <param name="path">parse(concat(__currentpath,/data.txt))</param>
+      <var onerror="break" name="dataDestination" source="text" type="datatable">
+        <param name="path">parse(concat(__currentpath,/SourceDetailedReport_{DATETIMESTAMP}.txt))</param>
         <param name="rowseperationchar">parse(__newline)</param>
         <param name="colseperationchar">parse(__tab)</param>
         <value><![CDATA[]]></value>
